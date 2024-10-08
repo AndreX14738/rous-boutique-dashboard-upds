b8851461d8a8eebe633228fc06f281e7bb1c4833
jose vargas <vargascamachojoseedwin@gmail.com>
2024-10-08 00:10:29 -0400

    jose edito el iniciar sesion en login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,7 @@
 function LoginPage() {
   return (
     <section className=" ">
-      <h1 className="text-4xl font-medium text-center py-8">Iniciar Sesion</h1>
+      <h1 className="text-4xl font-medium text-center py-8">Iniciar Sesion jose </h1>
       <form className="grid gap-4  w-96 grid-cols-1" action="">
         <label htmlFor="">Email</label>
         <input className="bg-zinc-600 px-4 py-2 rounded-md" type="email" />
